adada363706de9a4ba221da526461fb192231cbc
chishengqi <chishengqi@liepin.com>
2024-06-27 20:32:03 +0800

    feat: -

---
+++ 文件格式化/test/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useRef } from "react";
+import "./index.less";
+
+interface IProps {}
+
+const Test: React.FC<IProps> = () => {
+  return <div className="test-dxd-123"></div>;
+};
+
+export default Test;
