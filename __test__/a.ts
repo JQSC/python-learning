876e0722bd77a09792f7f224b34615757589bb5f
chishengqi <chishengqi@liepin.com>
2024-07-03 16:04:14 +0800

    更新目录结构

--- __test__/a.ts
+++
@@ -1,3 +0,0 @@
-export function isHtml(html: string): boolean {
-  return /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/.test(html);
-}
--- __test__/c/c.ts
+++
@@ -1,7 +0,0 @@
-function isString(str: any): boolean {
-  return typeof str === "string";
-}
-
-export function isNumber(str: any): boolean {
-  return typeof str === "number";
-}
