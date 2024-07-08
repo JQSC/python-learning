729deb5b49e411836d703bd73d01233b09dfe72b
chishengqi <chishengqi@liepin.com>
2024-07-08 11:38:55 +0800

    更新文件目录

---
+++ src/Python工具/查找特定文件并进行格式化/test/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from './index._less'
+import CalendarSvg from './image/icon-calendar.svg'
+
+const CalendarIcon = () => (
+  <>
+    <div className="user-antd-date-icon">
+      <CalendarSvg />
+    </div>
+    <style jsx>{styled}</style>
+  </>
+)
+
+export default CalendarIcon
