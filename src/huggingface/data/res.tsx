9cc02acd187c761a509de8619c9d2dc3ef1e3ba0
chishengqi <chishengqi@liepin.com>
2024-11-16 17:19:36 +0800

    feat: 使用大模型修改并覆盖原文件

--- src/huggingface/data/res.tsx
+++ src/huggingface/data/res.tsx
@@ -1,8 +1,12 @@
+import { getSafetyAgreement } from '@common/js/safetyAgreement';
+import { Form, FormInstance, Checkbox } from 'antd';
+import React from 'react';
+
 /**
- * AgreenChecked 组件，用于在表单中渲染一个协议接受的复选框
+ * AgreenChecked 组件，用于渲染并检查用户是否勾选了增值服务协议的复选框
  * @param {Object} props - 组件的属性对象
- * @param {FormInstance} props.form - form schema class 的实例
- * @returns {JSX.Element} - 返回包含协议接受复选框的表单组件
+ * @param {FormInstance} props.form - antd Form 组件实例，用于表单数据的收集和校验
+ * @returns {JSX.Element} - 返回渲染后的表单组件，其中包含一个复选框用于用户接受协议
  */
 const AgreenChecked = (props: { form: FormInstance }) => {
   return (
